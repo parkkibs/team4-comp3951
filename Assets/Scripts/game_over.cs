5e9339ba710d2b8795e563155be0acb01ec85c32
Sam <samerati2000@gmail.com>
2021-04-13 01:15:58 -0700

    Game Over screen added

--- Assets/Scripts/game_over.cs
+++ Assets/Scripts/game_over.cs
@@ -7,27 +7,21 @@ using UnityEngine.SceneManagement;
 /// @author Sam Merati
 /// @version 2.0
 /// </summary>
-public class main_menu : MonoBehaviour
+public class game_over : MonoBehaviour
 {
     /// <summary>
     /// Method for the New Game Button, redirects to the first level scene.
     /// </summary>
-    public void play_game()
+    public void restart_game()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
-    /// <summary>
-    /// Method for the Settings Button, redirects to the difficulty customization scene.
-    /// </summary>
-    public void game_settings()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
+
     /// <summary>
     /// Method for the Quit Game Button, closes the application.
     /// </summary>
     public void quit_game()
     {
-        Application.Quit();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
 }
